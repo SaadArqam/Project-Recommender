3a0b1a267525f7460981ff2c024de164ef645c67
Saad Arqam <saadarqamrnc@gmail.com>
2026-01-22 10:41:28 +0530

    feat: cleand the dataset: projects.csv

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -12,5 +12,16 @@ def clean_projects_data():
     txt_cols=["title","description","required_skills","tags"]
     for col in txt_cols:
         df[col]=(df[col].astype(str).str.lower().str.strip())
-        
+
+    df["required_skills"]=df["required_skills"].str.replace(r"\s*,\s*", ",", regex=True)
+    df["tags"]=df["tags"].str.replace(r"\s*,\s*", ",", regex=True)
+    df.drop_duplicates(subset="title",inplace=True)
+
+    processed_data_path.parent.mkdir(parents=True, exist_ok=True)
+    df.to_csv(processed_data_path,index=False)
+
+    print("Cleaned dataset and saved to",processed_data_path)
+
+if __name__=="__main__":
+    clean_projects_data()
     
